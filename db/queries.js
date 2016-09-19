fa3dc39a32a7604e6cc73c973d2d3402713b8076
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-19 14:31:37 -0600

    added complex query

--- db/queries.js
+++ db/queries.js
@@ -23,8 +23,24 @@ module.exports = {
     return Comments();
   },
   getSinglePost: function (id) {
-    return Posts().where('id', id)
-  }
-};
+    // return Posts().where('id', id)
+    return knex.raw(SELECT COUNT(*) FROM posts); //'raw implementation- use SQL instead of knex sytax'
+  },
+  getAllUserInteraction: function(id) {
+    userData = userData[0];
+      return Users().where('id', id)
+        .then(function(userData){
+          return Posts().where('user_id', id)
+            .then (function(postData){
+              userData.posts = postData;
+                return Comments().where('user_id', id)
+                  .then(function(commentData){
+                    userData.comments = commentData;
+                      return userData;
+                    })
+                })
+            })
+        }
+    };
 
 //functions are capitlized b/c they work similar to a contrstuctor

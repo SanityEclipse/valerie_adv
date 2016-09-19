1195bb0d276cec2bc154df7bd4dccba1ae178b4a
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-19 14:06:32 -0600

    added route to index.js and added queries

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../db/queries')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/posts/:id', function (res,req,next){
+  queries.getSinglePost(req.params.id);
+  .then(function(data){
+    res.render('index', {
+      data: data[0]
+    })
+  })
+})
 
 module.exports = router;
